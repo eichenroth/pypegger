7478262d836282aaa2ec79b11519c3fba9b3faec
Friedrich Schöne <friedrich.schoene@gmx.de>
2019-04-10 22:43:08 +0200

    added comments

--- peg_parser/rules.py
+++ peg_parser/rules.py
@@ -2,28 +2,59 @@ from .results import ParsingSuccess
 
 
 class Rule:
+    """
+    Abstract base class every rule inherits from.
+    This class should not be used directly to generate a grammar.
+    """
+
     def __init__(self):
         self.memoizationDict = {}
 
     def match(self, string):
+        """
+        Check if a prefix of a string matches the grammar defined by this rule.
+        :param string: The string to match.
+        :return: Boolean whether the string prefix matches or not.
+        """
         if self.parse(string):
             return True
         return False
 
     def match_whole(self, string):
+        """
+        Check if a whole string matches the grammar defined by this rule.
+        :param string: The string to match.
+        :return: Boolean whether the string matches or not.
+        """
         parse_result = self.parse(string)
         if parse_result and parse_result.end_pos == len(string):
             return True
         return False
 
     def parse(self, string, start_pos = 0):
+        """
+        Parses an input string to an abstract syntax tree.
+        :param string: The string to parse.
+        :param start_pos: Starting position within the string.
+        :return: The AST.
+        """
         return self.memoizationDict.setdefault((hash(string), start_pos), self._parse(string, start_pos))
 
     def _parse(self, string, start_pos):
+        """
+        Abstract method which is implemented by the subclasses doing the main parsing magic.
+        :param string: The string to parse.
+        :param start_pos: Starting position whithin the string.
+        :return: The AST.
+        """
         raise NotImplementedError
 
 
 class RuleCollection(Rule):
+    """
+    Abstract base class for rules that consist of multiple subrules.
+    """
+
     def __init__(self, *rules):
         super().__init__()
         self.rules = rules
@@ -40,6 +71,10 @@ class RuleCollection(Rule):
 
 
 class RuleWrapper(Rule):
+    """
+    Abstract base class for rules that consist of a single subrule.
+    """
+
     def __init__(self, rule):
         super().__init__()
         self.rule = rule
@@ -55,6 +90,10 @@ class RuleWrapper(Rule):
 
 
 class String(Rule):
+    """
+    Rule consisting of just a string to match.
+    """
+
     def __init__(self, s):
         assert type(s) == str
         super().__init__()
@@ -68,6 +107,10 @@ class String(Rule):
 
 
 class Choice(RuleCollection):
+    """
+    Prioritized choice rule, e.g, `(A | B | C)`.
+    """
+
     def _parse(self, string, start_pos):
         for rule in self.rules:
             rule_result = rule.parse(string, start_pos)
@@ -78,6 +121,10 @@ class Choice(RuleCollection):
 
 
 class Sequence(RuleCollection):
+    """
+    Sequence of rules, e.g. `A B C`.
+    """
+
     def _parse(self, string, start_pos):
         pos = start_pos
         children = []
@@ -92,6 +139,10 @@ class Sequence(RuleCollection):
 
 
 class And(RuleWrapper):
+    """
+    And (lookahead) rule that allows to check the string without consuming it, e.g. `&A`.
+    """
+
     def _parse(self, string, start_pos):
         rule_result = self.rule.parse(string, start_pos)
         if rule_result:
@@ -100,6 +151,10 @@ class And(RuleWrapper):
 
 
 class Not(RuleWrapper):
+    """
+    Not rule that checks the string if a rule is not applicable, e.g. `!A`.
+    """
+
     def _parse(self, string, start_pos):
         if self.rule.parse(string, start_pos):
             return False
@@ -107,6 +162,10 @@ class Not(RuleWrapper):
 
 
 class ZeroOrMore(RuleWrapper):
+    """
+    Zero or more rule, e.g. `A*`.
+    """
+
     def _parse(self, string, start_pos):
         pos = start_pos
         children = []
@@ -122,6 +181,10 @@ class ZeroOrMore(RuleWrapper):
 
 # maybe think this code over, adds one iteration over the children list
 class OneOrMore(RuleWrapper):
+    """
+    One or more rule, e.g. `A+`.
+    """
+
     def __init__(self, rule):
         super().__init__(rule)
         self.zero_or_more_rule = ZeroOrMore(rule)
@@ -138,6 +201,10 @@ class OneOrMore(RuleWrapper):
 
 
 class Optional(RuleWrapper):
+    """
+    Optional rule, e.g. `A?`.
+    """
+
     def _parse(self, string, start_pos):
         rule_result = self.rule.parse(string, start_pos)
         if rule_result:
