28f469838652ce69088d2fc74e34c58bbf41290c
Friedrich Schöne <friedrich.schoene@gmx.de>
2019-04-17 01:30:37 +0200

    added grammar test

---
+++ tests/test_rules.py
@@ -0,0 +1,26 @@
+import pytest
+
+from pegger.rules import *
+
+
+def test_grammar():
+    # {a^n b^n c^n | n \in N}
+    A = Choices()
+    A.add_rule(Sequence('a', A, 'b'))
+    A.add_rule('ab')
+    B = Choices()
+    B.add_rules(Sequence('b', B, 'c'), 'bc')
+    D = Choices(Not(Any()),  Sequence(And(Sequence(A, Not('b'))), ZeroOrMore('a'), B, Not(Any())))
+
+    grammar = D
+    assert grammar.match_whole('')
+    assert grammar.match_whole('aaabbbccc')
+    assert grammar.match_whole('aaaaaabbbbbbcccccc')
+
+    assert not grammar.match_whole('a')
+    assert not grammar.match_whole('b')
+    assert not grammar.match_whole('c')
+
+    assert not grammar.match_whole('aabbbccc')
+    assert not grammar.match_whole('aaabbccc')
+    assert not grammar.match_whole('aaabbbcc')
